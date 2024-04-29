c25ec964d6044643bc377369abc423baf787a4c6
Yogada Bhalerao <148211801+Yogab15@users.noreply.github.com>
2024-04-29 16:08:43 +0530

    Add files via upload

---
+++ Calculator3.java
@@ -0,0 +1,27 @@
+package MyGit;
+
+
+//METHOD OVERRIDDING
+ 
+class X
+{
+  protected int i = 1221;
+   
+  void methodOfX()
+  {
+      System.out.println(i);
+  }
+}
+
+public class Calculator3
+{
+  public static void main(String[] args)
+  {
+      X x = new X();
+       
+      System.out.println(x.i);
+       
+      x.methodOfX();
+  }
+}
+
